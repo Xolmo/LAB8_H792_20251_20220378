7783f375215b911d29b3744b05c9a02d9cf40a0c
Xolmo <a20220378@pucp.edu.pe>
2025-06-13 20:17:25 -0500

    Initial Host

---
+++ src/main/java/com/example/lab8_h792_20251_20220378/Lab8H7922025120220378Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_h792_20251_20220378;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab8H7922025120220378Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab8H7922025120220378Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab8_h792_20251_20220378/Lab8H7922025120220378ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_h792_20251_20220378;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab8H7922025120220378ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
