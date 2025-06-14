6ec7dc689d08fd77d9e94ff457569cf20e652edd
Xolmo <a20220378@pucp.edu.pe>
2025-06-13 21:31:32 -0500

    Ok v2

--- src/main/java/com/example/lab8_h792_20251_20220378/Repository/MonitoreoClimaticoRepository.java
+++ src/main/java/com/example/lab8_h792_20251_20220378/Repository/MonitoreoClimaticoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MonitoreoClimaticoRepository extends JpaRepository<Integer, MonitoreoClimatico> {
+public interface MonitoreoClimaticoRepository extends JpaRepository<MonitoreoClimatico, Integer> {
 }
